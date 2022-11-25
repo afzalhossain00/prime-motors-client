846c1d38c0eac7a9d9ad1260dc8d22fc0e9d74f6
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-11-25 21:33:12 +0600

    design sign up and created users

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const SignUp = () => {
-    return (
-        <div>
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    const { createUser } = useContext(AuthContext)
 
+    const handleSignUp = data => {
+        console.log(data);
+        createUser(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                toast.success('User created successfully')
+            })
+            .catch(error => console.log(error))
+    }
+
+    return (
+        <div className='h-[600px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='text-xl text-center'>Sign Up</h2>
+                <form onSubmit={handleSubmit(handleSignUp)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Name</span> </label>
+                        <input type='text'
+                            className='input input-bordered w-full max-w-xs'
+                            {...register("name", {
+                                required: 'Name is required'
+                            })}
+                        />
+                        {errors.name && <p className='text-red-600'>{errors.name.message}</p>}
+                        <label className="label"> <span className="label-text">Email</span> </label>
+                        <input type='email'
+                            className='input input-bordered w-full max-w-xs'
+                            {...register("email", {
+                                required: 'Email is required'
+                            })}
+                        />
+                        {errors.email && <p className='text-red-600'>{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"> <span className="label-text">Password</span></label>
+                        <input type='password'
+                            className='input input-bordered w-full max-w-xs'
+                            {...register("password", {
+                                required: 'Password is required',
+                                pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: "Password must have uppercase numbers and special characters" },
+                                minLength: { value: 6, message: 'Password must be 6 characters' }
+                            })}
+                        />
+                        {errors.password && <p className='text-red-600'>{errors.password.message}</p>}
+                    </div>
+                    <input className='btn btn-accent mt-4 w-full max-w-xs' value='Sign Up' type="submit" />
+                    {/* {signError && <p className='text-red-600'>{signError}</p>} */}
+                </form>
+                <p className='mt-3'>Already Have an account? <Link className='text-secondary' to='/login'>Please login</Link> </p>
+                <div className="divider">OR</div>
+                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+            </div>
         </div>
     );
 };
